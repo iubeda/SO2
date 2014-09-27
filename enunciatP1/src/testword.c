0c5290c9141dbdb28b6ada02a890f0bcaefd745c
hermetico <yojmarinv@gmail.com>
2014-09-27 12:31:18 +0200

    pruebas en codigos de transparencias

---
+++ enunciatP1/src/testword.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // per la funcio strlen
+#include <ctype.h>  // per les funcions isalpha, isdigit, ...
+
+#define MAXCHAR 100
+
+int main(void)
+{
+    int i, len;
+    char cadena[MAXCHAR];
+    char tokens[] = " ,;_-";
+    char *palabra, *copier;
+
+    printf("Introdueix la cadena a processar: ");
+    fgets(cadena, MAXCHAR, stdin);
+
+    len = strlen(cadena);
+    palabra = malloc(sizeof(char) * len);
+    copier = palabra;
+    i = 0;
+    while(*copier++ = cadena[i++]);
+
+    printf("Analizamos %s\n", palabra);
+    copier = strtok(cadena, tokens);
+    i = 0;
+    while(copier){
+        printf("%d: %s\n",i++, copier);
+        copier = strtok(NULL, tokens);
+    }
+
+}
+
