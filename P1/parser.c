a3970d367a587650f64593c95864b290535627b5
hermetico <yojmarinv@gmail.com>
2014-10-04 18:04:19 +0200

    comentarios i eliminacion de palabras con acentos o caracteres no iso

--- P1/parser.c
+++ P1/parser.c
@@ -2,17 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+// tamany maxim de paraula
 #define MAXBUFFER 100
+// tamany maxim de llistat de paraules, es podria solucionar amb una
+// llista enllasada
 #define MAXWORDS 1000
 #define APOSTROFE '\''
 #define ZERO '\0'
 
 
+
+/*
+ * Funcio que imprimeix una paraula per pantalla
+ */
 static void show_word(char *word)
 {
-    printf("len: %d word: %s\n", (int)(strlen(word)), word);
+    printf("Paraula valida: %s\n", word);
 }
 
+/*
+ * Funcio que imprimeix un llistat de paraules
+ */
 static void show_words(char *words[], int nwords)
 {
     int i = 0;
@@ -21,6 +32,9 @@ static void show_words(char *words[], int nwords)
 
 }
 
+/*
+ * Funcio que allibera de memoria les paraules
+ */
 static void free_words(char *words[], int nwords)
 {
     int i = 0;
@@ -35,13 +49,13 @@ static void free_words(char *words[], int nwords)
 char *allocword(char word[], int len)
 {
     char * newword;
-    // alocatamos len + 1 ya que no incluye el caracter \0
+    // allocatem len + 1 ja que len no inclueix el \0
     newword = malloc( (sizeof(char) * len) + 1 );
-    // volcamos la palabra con esta funcion, que le podemos indicar la longitud
+    // volquem la paraula amb strncpy, que ens permet definir la longitud a copiar
     strncpy(newword, word, len);
-    // ponemos el caracter \0 como ultimo
+    // posem l'ultim carater a ZERO
     newword[len] = ZERO;
-    // devolvemos el puntero
+    // retornem el punter
     return newword;
 }
 
@@ -54,7 +68,7 @@ char *allocword(char word[], int len)
  */
 int aarxiu(char *nomarxiu)
 {
-    int currentbff, nwords,caracter, numexception;
+    int currentbff, nwords,caracter, wordexception;
     int saveword;
     FILE *fl;
     char buffer[MAXBUFFER];
@@ -70,58 +84,67 @@ int aarxiu(char *nomarxiu)
 
     // inicialitzem el contador del buffer
     currentbff = 0;
-    numexception = 0;
+    // identifica excepcio de paraula
+    wordexception = 0;
+    // numero de paraules valides
     nwords = 0;
+    // identifica si hem d'emmagatzemar la paraula
     saveword = 0;
+    // llegim el primer caracter
     caracter = fgetc(fl);
+
     while( !feof(fl) && nwords < MAXWORDS)
     {
-        if(isalpha(caracter) && !numexception)
-        { // comprobamos si es una letra y no hay numexceptin
-            // comprobamos que este en lowercase
-            if(isupper(caracter))
+        if(isalpha(caracter)) // comprobem si es una lletra
+        {
+            if(!wordexception) // comprobem no tenim excepcio de numero
             {
-                caracter = tolower(caracter);
+                if(isupper(caracter))
+                {
+                    caracter =  tolower(caracter);
+                }
+                // emmagatzemem la lletra
+                buffer[currentbff++] = caracter;
             }
-            buffer[currentbff++] = caracter;
         }
         else if(isspace(caracter))
-        {   // desactivamos numexception si lo hubiese
-            if(numexception)numexception = 0;
-            // comprobamos que hay algo en el buffer
+        {   // desactivem excepcio de numero
+             wordexception = 0;
+            // comprovem que hi han caracters al buffer
             if(currentbff)
             {
-                // comprobamos que el ultimo caracter no sea un '
-                // si lo es, no lo tendremos en cuenta
-                if(buffer[currentbff-1] == APOSTROFE) currentbff--;
-            }
-            // volvemos a comprobar que hay algo en el buffer
-            if(currentbff)
-            {
-                saveword = 1;
+                // comprobem que l'ultim caracter no es apostrof
+                // si ho es, l'eliminem
+                if(buffer[currentbff-1] == APOSTROFE) 
+                    currentbff--;
+                // ara tornem a comprobar si n'hi han caracters
+                if(currentbff)
+                    saveword = 1;
             }
         }
         else if(isdigit(caracter))
         {
-            // comprobamos si es numero
-            // evitamos palabra con numero al inicio, o en medio
-            numexception = 1;
+            // evitem paraula amb numeros al inici, final o enmig
+            wordexception = 1;
             currentbff = 0;
         }
         else if(ispunct(caracter))
         {
-            if (caracter == APOSTROFE)
+            if (caracter == APOSTROFE && currentbff) // acceptem paraules amb apostrof
             {
-                if(currentbff)
-                { // si hay caracteres en el buffer, guardamos este tmb
-                    buffer[currentbff++] = caracter;
-                }
+                buffer[currentbff++] = caracter;
             }
-            else if(currentbff)
+            else if(currentbff) // Si no es apostrof no guardem el caracter pero si la paraula
             {
                 saveword = 1;
             }
+        }else
+        {
+            // si no es cap caracter conegut, el·liminem el buffer i activem excepcio
+            wordexception = 1;
+            currentbff = 0;
         }
+
         if(saveword)
         {
             saveword = 0;
