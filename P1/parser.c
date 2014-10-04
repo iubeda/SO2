1dcae8b8ee95bc52428749f4e1a41f009a2fa1ef
hermetico <yojmarinv@gmail.com>
2014-10-04 12:55:10 +0200

    añadidos comentarios e identado codigo

--- P1/parser.c
+++ P1/parser.c
@@ -5,20 +5,24 @@
 #define MAXBUFFER 100
 #define MAXWORDS 1000
 #define APOSTROFE '\''
+#define ZERO '\0'
 
 
-static void show_word(char *word){
+static void show_word(char *word)
+{
     printf("len: %d word: %s\n", (int)(strlen(word)), word);
 }
 
-static void show_words(char *words[], int nwords){
+static void show_words(char *words[], int nwords)
+{
     int i = 0;
     while(i < nwords)
         show_word(words[i++]);
 
 }
 
-static void free_words(char *words[], int nwords){
+static void free_words(char *words[], int nwords)
+{
     int i = 0;
     while(i < nwords)
         free(words[i++]);
@@ -28,14 +32,15 @@ static void free_words(char *words[], int nwords){
 /*
  * funcio que fa un malloc de la paraula en el array y retorna un punter
  */
-char *allocword(char word[], int len){
+char *allocword(char word[], int len)
+{
     char * newword;
     // alocatamos len + 1 ya que no incluye el caracter \0
     newword = malloc( (sizeof(char) * len) + 1 );
     // volcamos la palabra con esta funcion, que le podemos indicar la longitud
     strncpy(newword, word, len);
     // ponemos el caracter \0 como ultimo
-    newword[len] = '\0';
+    newword[len] = ZERO;
     // devolvemos el puntero
     return newword;
 }
@@ -43,8 +48,12 @@ char *allocword(char word[], int len){
 
 /**
  * Funcio que analitza un arxiu especificat per parametre
+ * i emmagatzema les paraules valides en un array static.
+ *
+ * 
  */
-int aarxiu(char *nomarxiu){
+int aarxiu(char *nomarxiu)
+{
     int currentbff, nwords,caracter, numexception;
     int saveword;
     FILE *fl;
@@ -53,7 +62,8 @@ int aarxiu(char *nomarxiu){
     
     //Obrim el arxiu en mode lectura
     fl = fopen(nomarxiu, "r");
-    if(!fl){
+    if(!fl)
+    {
         printf("No s'ha pogut obrir l'arxiu");
         exit(1);
     }
@@ -64,47 +74,58 @@ int aarxiu(char *nomarxiu){
     nwords = 0;
     saveword = 0;
     caracter = fgetc(fl);
-    while( !feof(fl) && nwords < MAXWORDS){
+    while( !feof(fl) && nwords < MAXWORDS)
+    {
         if(isalpha(caracter) && !numexception)
         { // comprobamos si es una letra y no hay numexceptin
             // comprobamos que este en lowercase
-            if(isupper(caracter)){
+            if(isupper(caracter))
+            {
                 caracter = tolower(caracter);
             }
             buffer[currentbff++] = caracter;
-        }else if(isspace(caracter))
+        }
+        else if(isspace(caracter))
         {   // desactivamos numexception si lo hubiese
             if(numexception)numexception = 0;
-            // comprobamos si es un espacio o salto de linea
             // comprobamos que hay algo en el buffer
-            if(currentbff){
+            if(currentbff)
+            {
                 // comprobamos que el ultimo caracter no sea un '
                 // si lo es, no lo tendremos en cuenta
                 if(buffer[currentbff-1] == APOSTROFE) currentbff--;
             }
             // volvemos a comprobar que hay algo en el buffer
-            if(currentbff){
-                saveword = 1;
-            }
-        }else if(isdigit(caracter)){ // comprobamos si es numero
-            // evitamos palabra con numero al inicio, o en medio
-            numexception = 1;
-            currentbff = 0;
-        }else if(ispunct(caracter)){
-            if (caracter == APOSTROFE){
-                if(currentbff){ // si hay caracteres en el buffer, guardamos este tmb
-                    buffer[currentbff++] = caracter;
-                }
-            }else if(currentbff){
+            if(currentbff)
+            {
                 saveword = 1;
             }
         }
-        if(saveword){
+        else if(isdigit(caracter))
+        {
+            // comprobamos si es numero
+            // evitamos palabra con numero al inicio, o en medio
+            numexception = 1;
+            currentbff = 0;
+        }
+        else if(ispunct(caracter))
+        {
+            if (caracter == APOSTROFE)
+            {
+                if(currentbff)
+                { // si hay caracteres en el buffer, guardamos este tmb
+                    buffer[currentbff++] = caracter;
+                }
+            }
+            else if(currentbff)
+            {
+                saveword = 1;
+            }
+        }
+        if(saveword)
+        {
             saveword = 0;
-
             words[nwords] = allocword(buffer, currentbff);
-            //printf("%s\n", words[nwords]);
-            //nwords--;
             currentbff = 0;
             nwords++;
         }
