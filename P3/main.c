06f75906f32429a43bf3c819f76c4725175554e5
IÑAKI UBEDA MARTINEZ <iubedama7.alumnes@ub052229.mat.ub.edu>
2014-10-30 17:38:24 +0100

    files for P3

---
+++ P3/main.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "include/const.h"
+#include "file-list.h"
+#include "file-parser.h"
+#include "linked-list.h"
+#include "hash-list.h"
+#include "red-black-tree.h"
+
+#define DEBUG 1
+#define DUMP_LIST 0
+#define DUMP_TREE 1
+
+typedef struct LONGEST{
+    int length; // longitud del la palabra
+    int file;   // numero de fitxer
+    char *word; // la palabra
+}Longest;
+
+Longest maslarga = {0, 0, NULL};
+
+
+
+/**
+ * Funcio que rep un tree i un arxiu procesar en format hash list 
+ * i l'indexa
+ * @ tree : arbre on s'indexa
+ * @ aproc : arxiu procesat a indexar
+ * @ fileid : numero d'arxiu a procesar
+ * */
+void indexar_en_llista_global(RBTree *tree, Hash_list *aproc, int num_arxius,  int arxiu)
+{
+    int i, len, j;
+    RBData *treeData;
+    ListItem *currentItem;
+    ListData *data;
+
+    
+    // recorremos cada posicion de la lista hash
+    for(i = 0; i < aproc->length; i++)
+    {
+        if(DUMP_LIST){
+            printf("Dumping list %i, file %i\n", i, arxiu);
+            dumpList(aproc->data[i]);
+            printf("*\t*\t*\n");
+        }
+        //indexamos el contenido de esta lista enlazada
+        //indexar_llista_enllasada(tree, aproc->data[i], aproc->length);
+
+        //recuperem el primer node de la llista
+        currentItem = aproc->data[i]->first;
+        while(currentItem != NULL)
+        {
+            data = currentItem->data;
+            //busquem la paraula a l'arbre
+            treeData = findNode(tree, data->primary_key);
+            if(treeData != NULL) // si la paraula esta a l'arbre
+            {
+                // augmentem en 1 el numero d'arxius en el que surt la paraula
+                // i marquem el numero de cops que surt a l'arxiu
+                treeData->numFiles++;
+                treeData->numTimes[arxiu] = data->numTimes;
+
+            }
+            else
+            {   
+                // si no esta, hem d'allocatar, inicialitzar el data
+                // copiar la paraula, copiar el numero de cops per arxiu
+                // i aumengtar el numer d'arxus 
+                treeData = malloc(sizeof(RBData));
+
+                len = strlen(data->primary_key);
+                initRBData(treeData, len,num_arxius);
+                strcpy(treeData->primary_key, data->primary_key);
+                treeData->numTimes[arxiu] = data->numTimes;
+                treeData->numFiles++;
+                insertNode(tree, treeData);
+                
+                
+                // si esta palabra es mas larga que la almacenada como larga,
+                // la guardamos
+                if(maslarga.length < len)
+                {
+                    maslarga.length = len;
+                    maslarga.file = arxiu; // solo almacena el primer archivo
+                    maslarga.word = treeData->primary_key;
+                }
+                
+            }
+            currentItem = currentItem->next;
+
+        }
+    }
+}
+
+/**
+ * procesa un llistat d arxius
+ */
+int processar_llista_arxius(Str_array *arxius, RBTree *tree)
+{
+    int i,j, numarxius;
+    Hash_list *arxiu_procesat;
+
+    numarxius = arxius->length;
+    
+    // recorrem el llistat d'arxius
+    for( i = 0; i < numarxius; i++ )
+    {
+        FILE *fl;
+        fl = fopen(arxius->data[i], "r");
+        // si no podem obrir l'arxiu, pasem al seguent
+        if(!fl) continue;
+
+        arxiu_procesat = fparser(fl);
+        // un cop tenim l'arxiu procesat en una hash_list, l'indexem
+        indexar_en_llista_global(tree, arxiu_procesat, arxius->length,  i);
+
+        fclose(fl);
+        
+        // alliberem memoria
+        free_hash_list(arxiu_procesat);
+        free(arxiu_procesat);
+
+    }
+
+}
+
+
+/**
+ *
+ * Main function
+ *
+ */
+int main(int argc, char **argv)
+{
+    Str_array *paraules;
+    RBTree tree;
+    RBData *data;
+    int iter;
+   
+
+    /* comprobem si hi ha parametre d'entrada */
+    if (argc != 2)
+    {
+        printf("Exemple d'us: %s fitxer.cfg\n", argv[0]);
+        exit(1);
+    }
+    
+    // iniciamos el arbol
+    initTree(&tree);
+    
+    /* cridem a la funcio del llistat de paraules que retorna un struct */
+    paraules = flist(argv[1]);
+    processar_llista_arxius(paraules, &tree);
+
+
+    
+  
+    if(DUMP_TREE) dumpTree(&tree);
+    
+    printf("Palabra mas larga: %s\n"
+        "de longitud: %i\naparece en el fichero: %s\n",
+        maslarga.word, maslarga.length, paraules->data[maslarga.file]);
+    
+    /* Delete the tree */
+    deleteTree( &tree );
+    
+    // delete paraules
+    for(iter = 0; iter < paraules->length; iter++)
+    {
+        free(paraules->data[iter]);
+    }
+    free(paraules->data);
+    free(paraules);
+
+    return 0;
+}
