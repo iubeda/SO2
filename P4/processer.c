d3f948f79965f0510f63596867c665a24fc6085a
hermetico <yojmarinv@gmail.com>
2014-12-01 23:52:45 +0100

    añadidos mensajes de debug, bloqueo antes de indexar, modificada la funcion pnext(), estaba mal implementada

--- P4/processer.c
+++ P4/processer.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "file-parser.h"
+#include "include/debug.h"
 #include "indexer.h"
 
 Processer_conf p_conf = { 0, 0, 0 };
@@ -31,15 +32,24 @@ static int pnext()
 {
     int num;
 
+    pthread_t tid = pthread_self();
+    
     //exclusio
     pthread_mutex_lock(&mutex_processer);
+    if(DEBUGPTH)
+        printf("Thread %i demana arxiu toca %i, l'ultim es %i\n",(int)tid, p_conf.k, p_conf.last);
     num = p_conf.k++;
+    if(DEBUGPTH)
+        printf("Thread %i rep arxiu %i, k queda %i\n",(int)tid, num, p_conf.k);
     pthread_mutex_unlock(&mutex_processer);
     //fi exclusio
 
-    if(num > p_conf.last)
+    if(num >= p_conf.last)
+    {
+        if(DEBUGPTH)
+            printf("Thread %i ja no procesara mes\n",(int)tid);
         num = -1;
-
+    }
     return num;
 }
 
@@ -51,12 +61,18 @@ static int pnext()
  */
 void *procesador(void *arg)
 {
+    if(!p_conf.init)
+    {
+        printf("Error: Processer scheme should be initialized before call procesador function\n");
+        return NULL;
+    }
+
     Hash_list *arxiu_procesat;
     Processer_context *context = (Processer_context *) arg;
 
     Str_array *arxius = context->llista;
     RBTree *tree = context->tree;
-
+    pthread_t tid = pthread_self();
     int narxiu;
     FILE *fl;
 
@@ -66,16 +82,25 @@ void *procesador(void *arg)
     // is in tthe position 0
     while( (narxiu = pnext()) != -1 )
     {
+        if(DEBUGPTH)
+            printf("Thread %i processant arxiu %i\n",(int)tid, narxiu);
+
         fl = fopen(arxius->data[narxiu], "r");
+
         arxiu_procesat = fparser(fl);
 
-        //TODO call indexar_llista_golbal
-        //pthread_mutex_lock(&pmutex_indexer);
-            // un cop tenim l'arxiu procesat en una hash_list, l'indexem
-            indexar_en_llista_global(tree, arxiu_procesat, arxius->length,  narxiu);
-        //pthread_mutex_unlock(&pmutex_indexer);
-
         fclose(fl);
+
+        //TODO call indexar_llista_golbal
+        pthread_mutex_lock(&pmutex_indexer);
+            if(DEBUGPTH)
+                printf("Thread %i demana indexar \n",(int)tid);
+            // un cop tenim l'arxiu procesat en una hash_list, l'indexem
+        indexar_en_llista_global(tree, arxiu_procesat, arxius->length,  narxiu);
+            if(DEBUGPTH)
+                printf("Thread %i termina d'indexar\n",(int)tid);
+        pthread_mutex_unlock(&pmutex_indexer);
+
         free_hash_list(arxiu_procesat);
         free(arxiu_procesat);
     }
